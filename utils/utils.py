58cb3bf297dd5adfd6516988630b157b003229b4
TthBnc <bence.zltn.toth@gmail.com>
2023-10-24 00:01:21 +0200

    new models+lfs, eval notebook

--- utils/utils.py
+++ utils/utils.py
@@ -25,7 +25,7 @@ def slice_video(video_name_ext, frame_interval, path, write_path = 'C:\\_TBZ min
         if (frame_count % frame_interval == 0):
             filename = f'{video_name}_{frame_count//frame_interval}.jpg'
             file_path = os.path.join(write_path, filename)
-            print(file_path)
+            # print(file_path)
             if frame is None:
                 print(f'frame is None')
             else:
@@ -58,7 +58,7 @@ def delete_temp_contents(temp_folder='C:\\_TBZ mindenes\\sewerbot_ai\\src\\pictu
         print(f'Error deleting contents in {temp_folder}: {e}')
 
 
-def process_video(video_name_ext, frame_interval=1, src=None, special_frames=None, throwaway_frames=None):
+def process_video(video_name_ext, frame_interval=1, src=None, special_frames=None, throwaway_frames=None, base_write_path=None):
     if src is None:
         src = 'C:\\_TBZ mindenes\\sewerbot_ai\\src\\videos'
     if special_frames is None:
@@ -92,12 +92,53 @@ def process_video(video_name_ext, frame_interval=1, src=None, special_frames=Non
                 for start, end in special_frames:
                     if end >= frame_count >= start:
                         filename = f'end_{video_name}_{frame_count//frame_interval}.jpg'
-                        write_path = 'C:\\_TBZ mindenes\\sewerbot_ai\\src\\pictures\\data\\end'
+                        write_path = os.path.join(base_write_path, "end")
+                        # write_path = 'C:\\_TBZ mindenes\\sewerbot_ai\\src\\pictures\\data\\end'
                         not_in_range = False
                 if not_in_range:
                     filename = f'notEnd_{video_name}_{frame_count//frame_interval}.jpg'
-                    write_path = 'C:\\_TBZ mindenes\\sewerbot_ai\\src\\pictures\\data\\notEnd'
+                    write_path = os.path.join(base_write_path, 'notEnd')
+                    # write_path = 'C:\\_TBZ mindenes\\sewerbot_ai\\src\\pictures\\data\\notEnd'
                 file_path = os.path.join(write_path, filename)
                 cv2.imwrite(file_path, frame)
 
     video.release()
+
+
+import torch
+def save_model(epoch, model, optimizer, filename, isCheckpoint=False):
+    """
+    usage:
+        save_checkpoint(epoch, gen, opt_gen, "generator_checkpoint.pth")
+        save_checkpoint(epoch, critic, opt_critic, "critic_checkpoint.pth")
+    """
+    if isCheckpoint:
+        path = os.path.join("models", "checkpoints", filename + '.pth')
+    else:
+        path = os.path.join("models", filename + '.pth')
+
+    checkpoint = {
+        "epoch": epoch,
+        "model_state": model.state_dict(),
+        "optimizer_state": optimizer.state_dict(),
+    }
+    if isCheckpoint: print('=> Saving checkpoint')
+    else: print('=> Saving model')
+    torch.save(checkpoint, path)
+
+def load_model(model, optimizer, filename):
+    """
+    usage:
+        start_epoch = load_checkpoint(gen, opt_gen, "generator_checkpoint.pth")
+        start_epoch = max(start_epoch, load_checkpoint(critic, opt_critic, "critic_checkpoint.pth"))
+
+        for epoch in range(start_epoch, NUM_EPOCHS):
+            # ...
+            # your training code here
+            # ...
+    """
+    
+    checkpoint = torch.load(filename)
+    model.load_state_dict(checkpoint["model_state"])
+    optimizer.load_state_dict(checkpoint["optimizer_state"])
+    return checkpoint["epoch"]  # You can use this to know where the training left off
